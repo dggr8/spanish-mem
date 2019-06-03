0dd933d4293d48687ec6208bd117fd864047e3f6
Dhanya Ganesh <dgaaaaaaaaaa@gmail.com>
2019-06-02 22:08:42 -0600

    list_commands implementation

--- src/file_operations/file_ops.go
+++ src/file_operations/file_ops.go
@@ -1,9 +1,11 @@
 package file_operations
 
 import (
-  "encoding/csv"
+	"encoding/csv"
+	"fmt"
   "path/filepath"
-  "os"
+	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -29,4 +31,42 @@ func LoadFiles(glob_path string) int64 {
 	}
 
 	return number_of_words
+}
+
+func ListFiles(glob_path string) {
+	// Print a list of files that tests use.
+	all_files, err := filepath.Glob(glob_path)
+	check(err)
+	fmt.Println("------------------------------------")
+	for _, filepath := range all_files {
+		fmt.Println(formatFilename(filepath))
+	}
+	fmt.Println("------------------------------------")
+}
+
+func formatFilename(file_path string) string {
+	parts := strings.Split(file_path, "/")
+	csvfile := parts[len(parts)-1]
+	return csvfile[:len(csvfile)-4]
+}
+
+func ListWords(glob_path string) {
+	// Print a list of words that tests use.
+	all_files, err := filepath.Glob(glob_path)
+	check(err)
+	fmt.Println("------------------------------------")
+	count := 1
+	for _, filepath := range all_files {
+		file, err := os.Open(filepath)
+		check(err)
+		defer file.Close()
+
+		lines, err := csv.NewReader(file).ReadAll()
+		check(err)
+		for _, line := range lines {
+			fmt.Println(count, line[0], "==", line[1])
+			count = count + 1
+		}
+	}
+	fmt.Println("------------------------------------")
 }
