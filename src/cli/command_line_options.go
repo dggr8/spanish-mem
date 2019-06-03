6fab2451146d7bb5f5fb53b241109558cb71b05e
Dhanya Ganesh <dgaaaaaaaaaa@gmail.com>
2019-06-02 20:32:31 -0600

    CLI is working now!

--- src/cli/command_line_options.go
+++ src/cli/command_line_options.go
@@ -1,4 +1,31 @@
 package cli
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var list_of_commands = map[string]bool{
+	"list files": true,
+	"list words": true,
+	"train spanish": true,
+	"train english": true,
+	"results": true,
+	"exit": true,
+}
+
 func GetCommand() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What do you want to do now?")
+	fmt.Print(">")
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+	if list_of_commands[text] == true {
+		return text
+	} else {
+		fmt.Println("I'm sorry. I didn't get that.")
+		return ""
+	}
 }
--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "github.com/dggr8/spanish-mem/src/cli"
   "github.com/dggr8/spanish-mem/src/file_operations"
 )
 
@@ -10,4 +11,8 @@ func main() {
   const file_glob string = "../data/*.csv"
   number_of_words := file_operations.LoadFiles(file_glob)
   fmt.Println("I have", number_of_words, "words!")
+  var command string
+  for command != "exit" {
+    command = cli.GetCommand()
+  }
 }
